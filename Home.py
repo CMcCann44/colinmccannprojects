87d73479189f3b8f5078f0fb660791f4696d6e68
Colin McCann <colinmccann44@gmail.com>
2024-10-03 15:35:18 -0400

    Deployment updates

--- Home.py
+++ Home.py
@@ -2,6 +2,11 @@ import streamlit as st
 from PIL import Image
 
 # streamlit run Home.py
+# git add .
+# git commit -m "message"
+# git branch -M main
+# git push -u origin main
+
 # Emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
 st.set_page_config(page_title='Colin McCann', page_icon=':desktop_computer:', layout='wide')
 
@@ -12,8 +17,11 @@ def local_css(file_name):
 
 # Load Assets
 fig1 = Image.open('images/placeholder.png')
-# fig2 = Image.open('images/name.png')
-# fig3 = Image.open('images/name.png')
+airline_fig = Image.open('images/AirportFig1.png')
+day_of_week_fig = Image.open('images/AirportFig2.png')
+stock_fig = Image.open('images/stockgraph.png')
+minecomputer_fig = Image.open('images/FullAdder.png')
+# fig4 = Image.open('images/name.png')
 
 
 # === Header ===
@@ -34,43 +42,41 @@ with st.container():
     text_column, image_column = st.columns(2)
     with text_column:
         st.subheader('Airport Flight Data Analysis')
-        st.write('Used Pandas to analyze large JFK airport datasets with >100,000 entries and determine possible causes of flight delays.')
+        st.write('Used Pandas to analyze large JFK airport datasets with >25,000 entries and determine possible causes of flight delays.')
         st.write('Performed chi-square and t-tests to determine if certain factors were significantly correlated with flight delay, '
                  'such as day of the week, flight destination, airline, etc.')
     with image_column:
-        st.image(fig1)
+        st.image(airline_fig)
 
 # --- Stock Market Web Scraping ---
 with st.container():
     text_column, image_column = st.columns(2)
     with text_column:
         st.subheader('Stock Market Web Scraping')
-        st.write('Used yFinance and BeautifulSoup to scrape and parse web stock data. '
-                 'Also used Pandas and Plotly to create graphs showing stock trends.')
+        st.write('Used yFinance and BeautifulSoup to scrape and parse web stock data.')
+        st.write('Also used Pandas and Plotly to create graphs showing stock trends.')
     with image_column:
-        st.image(fig1)
+        st.image(stock_fig)
 
 # --- Minecraft Computer ---
 with st.container():
     text_column, image_column = st.columns(2)
     with text_column:
         st.subheader('Minecraft Computer')
-        st.write('Built original design for a 4-bit adder and register using in-game wiring system. Working on an original 16-bit CPU.')
+        st.write('Built original adder and register using in-game wiring system.')
+        st.write('Working on an original 4-bit CPU.')
     with image_column:
-        st.image(fig1)
+        st.image(minecomputer_fig)
 
 # --- Ellipsoid Recoding for Classification Machine Learning ---
 with st.container():
-    text_column, image_column = st.columns(2)
-    with text_column:
-        st.subheader('Ellipsoid Recoding for Classification Machine Learning Models (current)')
-        st.write('As part of research projects with Dr. Singh and Dr. Kurtz at Binghamton University. '
-                 'Working on an ellipse-based recoding method and a pairwise relational feature recoding.')
-    with image_column:
-        st.image(fig1)
+    st.subheader('Ellipsoid Recoding for Classification Machine Learning Models (current)')
+    st.write('As part of research projects with Dr. Singh and Dr. Kurtz at Binghamton University.')
+    st.write('Working on an ellipse-based recoding method and a pairwise relational feature recoding.')
 
 
 # --- Probability Distribution Generator ---
+
 
 
 # === BU ===
@@ -79,7 +85,7 @@ with st.container():
     st.write('---')
     st.header('College')
     st.write('Math/data science major, CS minor at Binghamton University')
-    st.subheader('Courses Taken:')
+    st.subheader('Courses Taken')
     st.write('Calculus I-III, Linear Algebra, Number Systems, '
             'Programming and Hardware Fundamentals, Probability Theory')
 
@@ -87,7 +93,7 @@ with st.container():
 with st.container():
     st.write('---')
     st.header('Coursera')
-    st.write('Learned Python for data science through 5 courses.')
+    st.write('Learned Python for data science through 5 courses. Certificates shown on LinkedIn.')
 
 # === Footer ===
 with st.container():
--- pages/More.py
+++ pages/More.py
@@ -3,13 +3,34 @@ from PIL import Image
 
 # --- LOAD ASSETS
 fig1 = Image.open('images/placeholder.png')
+full_adder_fig = Image.open('images/FullAdder.png')
+mux_fig = Image.open('images/Mux.png')
+# sr_latch_fig = Image.open('images/SRLatch.png')
+# register_fig = Image.open('images/Register.png')
 # img_name = Image.open('images/name.png')
 
 
 # --- HEADER
 with st.container():
-    st.title('Source Code')
+    st.title('More Detail on Current Projects')
+    st.header('Minecraft Computer')
+    text_column1, text_column2 = st.columns(2)
+    with text_column1():
+        st.subheader('Original Full Adder')
+        st.image(full_adder_fig)
+    with text_column2():
+        st.subheader('Original Multiplexer')
+        st.image(mux_fig)
+
+#with st.container():
+    #text_column1, text_column2 = st.columns(2)
+    #with text_column1():
+        #st.subheader('Original SR Latch')
+        #st.image(sr_latch_fig)
+    #with text_column2():
+        #st.subheader('Original Register')
+        #st.image(register_fig)
 
 with st.container():
-    st.header('Prerequisite Code')
-    st.image(fig1)
+    st.header('Ellipse Recoding')
+    st.subheader('More info to come')
